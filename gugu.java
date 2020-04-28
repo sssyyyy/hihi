7f79958fa57ab4abeb28a89ae82da30cbddae056
psy <thdud4548@naver.com>
2020-04-28 19:27:07 +0900

    version 1.5

--- gugu.java
+++ gugu.java
@@ -7,11 +7,17 @@ public class gugu {
 		Scanner in =new Scanner(System.in);
 		int num = in.nextInt();
 		
+		if(num<0)
+		{
 		for(int i = 1;i<10;i++)
 		{
-			System.out.println(num+" * "+i+" = "+num*i);
+			System.out.println(i+"단");
+			for(int j=1;j<10;j++)
+				System.out.println(i+" * "+j+" = "+i*j);
 		}
-		
+		}
+
+	}
 
 	}
 
